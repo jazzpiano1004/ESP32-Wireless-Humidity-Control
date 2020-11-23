22179209fd35594982fd1ca5b388a34439dd68e8
nekomata <jazzpiano1004@gmail.com>
2020-11-24 00:26:45 +0700

    sensor_node : [Add] IO description for TFT_eSPI in tft_lcd.h

--- src/sensor_node/tft_lcd.h
+++ src/sensor_node/tft_lcd.h
@@ -13,6 +13,15 @@
 #include <TFT_eSPI.h>
 #include <JPEGDecoder.h>
 
+/* This should be setup at TFT_eSPI library folder
+ *  
+ */
+//#define TFT_MISO 19
+//#define TFT_MOSI 23
+//#define TFT_SCLK 18
+//#define TFT_CS   15  // Chip select control pin
+//#define TFT_DC   26  // Data Command control pin
+//#define TFT_RST  25  // Reset pin (could connect to RST pin)
 
 extern TFT_eSPI tft;
 
